3cd3d9c956f4a637f396498d448044d1b0c8f724
Mathys <mathys.deshaies@icloud.com>
2024-05-02 23:24:14 -0400

    feat: use new component LinkedinBasicInfo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import GithubBasicInfo from "@/components/GithubBasicInfo";
+import LinkedinBasicInfo from "@/components/LinkedinBasicInfo";
 
 export default function Home() {
   return (
     <main>
-      <GithubBasicInfo />
+      <div className="space-y-10 py-4 px-8">
+        <GithubBasicInfo />
+        <LinkedinBasicInfo />
+      </div>
     </main>
   );
 }
