e601bf341596507d0b2132d935140505ff566213
Mathys <mathys.deshaies@icloud.com>
2024-05-08 20:54:06 -0400

    fix: move getGithubInfo in a util

---
+++ src/lib/types/gitHubInfo.tsx
@@ -0,0 +1,12 @@
+export interface GithubUserInfo {
+  public_repos: number;
+  followers: number;
+  following: number;
+  avatar_url: string;
+  id: number;
+  login: string;
+  name: string;
+  bio: string;
+  location: string;
+  created_at: string;
+}
