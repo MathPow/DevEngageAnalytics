295bf5e9fcd5a6186a4a1c54935d6315fe555af1
Mathys <mathys.deshaies@icloud.com>
2024-05-12 18:42:00 -0400

    feat: add template file with i18n config

---
+++ src/app/template.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import i18n from "i18next";
+import "../i18n";
+import React from "react";
+import { initReactI18next } from "react-i18next";
+import frTranslation from "@/public/locales/fr.json";
+import enTranslation from "@/public/locales/en.json";
+
+interface ConfigurationProps {
+  children: React.ReactNode;
+}
+
+i18n.use(initReactI18next).init({
+  resources: {
+    fr: { translation: frTranslation },
+    en: { translation: enTranslation },
+  },
+  lng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+const Configuration = ({ children }: ConfigurationProps) => {
+  return <div id="root">{children}</div>;
+};
+
+export default Configuration;
