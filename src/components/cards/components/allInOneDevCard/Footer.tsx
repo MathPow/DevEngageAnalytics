3989fb5ee042b5daa1e764ddfb757fbd0fe0df61
Mathys <mathys.deshaies@icloud.com>
2024-05-14 23:40:46 -0400

    feat: update Footer Component

--- src/components/cards/components/allInOneDevCard/Footer.tsx
+++ src/components/cards/components/allInOneDevCard/Footer.tsx
@@ -2,7 +2,9 @@
 
 import Icon from "@/components/Icon";
 import { Separator } from "@/components/ui/separator";
+import { formatDateClassic } from "@/lib/composables/formatDate";
 import { Optional } from "@/lib/types/optional";
+import { useTranslation } from "react-i18next";
 
 interface FooterProps {
   githubUrl?: string;
@@ -11,7 +13,12 @@ interface FooterProps {
   websiteUrl?: string;
   figmaUrl?: string;
   behanceUrl?: string;
-  switchPage?: () => void;
+  githubJoinedDate?: Date;
+  gitlabJoinedDate?: Date;
+  linkedinJoinedDate?: Date;
+  isFirstPage?: boolean;
+  togglePage?: () => void;
+  className?: string;
 }
 
 export default function Footer({
@@ -19,15 +26,53 @@ export default function Footer({
   gitlabUrl,
   linkedinUrl,
   websiteUrl,
+  githubJoinedDate,
+  gitlabJoinedDate,
+  linkedinJoinedDate,
+  isFirstPage,
+  togglePage,
+  className = "",
 }: FooterProps) {
+  const { t } = useTranslation();
+
   function openWindow(url: Optional<string>) {
     if (url) {
       window.open(url, "_blank");
     }
   }
 
+  function findFurthestDate() {
+    const dates: Date[] = [
+      githubJoinedDate,
+      gitlabJoinedDate,
+      linkedinJoinedDate,
+    ]
+      .filter((date) => date !== undefined)
+      .map((dateStr) => dateStr as Date);
+
+    if (dates.length === 0) {
+      return undefined;
+    }
+
+    const furthest = Math.max(...dates.map((date) => date.getTime()));
+    return dates.find((date) => date.getTime() === furthest);
+  }
+
+  function getFormatedJoinedDate() {
+    const date = findFurthestDate();
+    if (date) {
+      return formatDateClassic(date);
+    }
+    return "";
+  }
+
   return (
-    <div className="absolute w-full px-8 bottom-4">
+    <div className={className}>
+      {(githubJoinedDate || gitlabJoinedDate || linkedinJoinedDate) && (
+        <p className="text-xs text-_lightGrayText">
+          {t("card.info.joined_date")} {getFormatedJoinedDate()}
+        </p>
+      )}
       <Separator className="bg-_darkGrayText mb-2" />
       <div className="flex justify-between">
         <div className="flex gap-x-3">
@@ -35,6 +80,7 @@ export default function Footer({
             <Icon
               className="hover:cursor-pointer"
               name={"github"}
+              size={26}
               onClick={() => {
                 openWindow(githubUrl);
               }}
@@ -44,8 +90,19 @@ export default function Footer({
             <Icon
               className="hover:cursor-pointer"
               name={"linkedin"}
+              size={26}
               onClick={() => {
                 openWindow(linkedinUrl);
+              }}
+            />
+          )}
+          {gitlabUrl && (
+            <Icon
+              className="hover:cursor-pointer"
+              name={"gitlab"}
+              size={26}
+              onClick={() => {
+                openWindow(gitlabUrl);
               }}
             />
           )}
@@ -53,19 +110,21 @@ export default function Footer({
             <Icon
               className="hover:cursor-pointer"
               name={"web"}
+              size={26}
               onClick={() => {
                 openWindow(websiteUrl);
               }}
             />
           )}
         </div>
-        <Icon
-          className="hover:cursor-pointer"
-          name={"chevron"}
-          onClick={() => {
-            openWindow(githubUrl);
-          }}
-        />
+        {isFirstPage !== undefined && (
+          <Icon
+            className={`hover:cursor-pointer ${!isFirstPage && "rotate-180"}`}
+            name={"chevron"}
+            size={26}
+            onClick={togglePage}
+          />
+        )}
       </div>
     </div>
   );
