b558466b0ce3cf8f833673ccafa78de788729f26
Mathys <mathys.deshaies@icloud.com>
2024-05-14 23:42:03 -0400

    feat: export new components

--- src/components/cards/components/allInOneDevCard/index.ts
+++ src/components/cards/components/allInOneDevCard/index.ts
@@ -1,3 +1,6 @@
 export { default as Header } from "./Header";
 export { default as Bio } from "./Bio";
 export { default as Footer } from "./Footer";
+export { default as Experience } from "./Experience";
+export { default as GitStats } from "./GitStats";
+export { default as GitGraph } from "./GitGraph";
