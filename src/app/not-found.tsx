ae2873971aa18448c9e4bf04ca88fff67c337f15
Mathys <mathys.deshaies@icloud.com>
2024-05-18 20:46:52 -0400

    feat: make the 0 in 404 a casino chip

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,8 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
+import CasinoChip from "@/public/assets/img/casino-chip.png";
 
 export default function NotFound() {
   const { t } = useTranslation();
@@ -12,7 +14,12 @@ export default function NotFound() {
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
           <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
-            <span className="sr-only">Error</span>404
+            <span className="sr-only">Error</span>
+            <span className="text-red-500 flex justify-center">
+              <span>4</span>
+              <Image className="size-32" src={CasinoChip} alt={"0"} />
+              <span>4</span>
+            </span>
           </h2>
           <p className="text-2xl font-semibold md:text-3xl">{t("ui.not_found.could_not_find_page")}</p>
           <p className="mt-4 mb-8 dark:text-gray-600">{t("ui.not_found.do_not_worry")}</p>
