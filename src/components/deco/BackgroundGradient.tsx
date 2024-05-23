ea1cd026028a84e23b195b6eed420411d88fa3f6
Mathys <mathys.deshaies@icloud.com>
2024-05-22 21:52:12 -0400

    feat: add BackgroundGradient

---
+++ src/components/deco/BackgroundGradient.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function BackgroundGradient() {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 -top-96 bg-[radial-gradient(circle_580px_at_120%_30px,#ffc2ba,transparent)] md:bg-[radial-gradient(circle_800px_at_90%_20px,#ffc2ba,transparent)] lg:bg-[radial-gradient(circle_900px_at_90%_80px,#ffc2ba,transparent)] -z-10"></div>
+  );
+}
