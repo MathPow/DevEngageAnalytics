0f92ce0928b790ef4dbb834a1e6a04f9ca75dbe9
Mathys <mathys.deshaies@icloud.com>
2024-05-27 15:47:06 -0400

    fix: make logo fit darkmode in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Logo from "@/../public/logo.svg";
+import LogoDark from "@/../public/LogoDark.svg";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme } = useTheme();
   return (
     <main className="flex justify-center mt-32">
       <div className="flex flex-col">
-        <Logo className="size-96" />
+        {theme === "dark" ? <LogoDark className="size-96" /> : <Logo className="size-96" />}
         <p className="font-bold text-center animate-pulse">Project in progress...</p>
       </div>
     </main>
