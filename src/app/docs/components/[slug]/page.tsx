23ef8445164363099cb306b6fd3c164c700e866d
Mathys <mathys.deshaies@icloud.com>
2024-05-27 11:34:08 -0400

    fix: tell the valid slug for dynamic routing

--- src/app/docs/components/[slug]/page.tsx
+++ src/app/docs/components/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import BackgroundGradient from "@/components/deco/BackgroundGradient";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 interface ContentProps {
   params: {
@@ -6,10 +7,39 @@ interface ContentProps {
   };
 }
 
-export default function Content({ params }: ContentProps) {
+const validSlugs = [
+  "allinonedev",
+  "gitlover",
+  "darkmode",
+  "allinonedesigner",
+  "certificatesflex",
+  "beautifulasymmetric",
+];
+
+export async function generateStaticParams() {
+  return validSlugs.map((slug) => ({
+    slug,
+  }));
+}
+
+export async function generateMetadata({ params }: ContentProps): Promise<Metadata> {
+  const { slug } = params;
+
+  return {
+    title: `Page - ${slug.charAt(0).toUpperCase() + slug.slice(1)}`,
+  };
+}
+
+export default function Page({ params }: ContentProps) {
+  const { slug } = params;
+
+  if (!validSlugs.includes(slug)) {
+    notFound();
+  }
+
   return (
     <main>
-      <p>{params.slug}</p>
+      <p>{slug}</p>
     </main>
   );
 }
