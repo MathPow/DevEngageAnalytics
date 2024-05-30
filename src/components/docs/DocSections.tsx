835294518fbdfd20c3878550f856eb12cb1c2bd5
Mathys <mathys.deshaies@icloud.com>
2024-05-29 23:37:34 -0400

    feat: add DocSections component

---
+++ src/components/docs/DocSections.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+interface DocSectionsProps {
+  slug: string;
+  url: string;
+}
+
+export default function DocSections({ slug, url }: DocSectionsProps) {
+  const { t } = useTranslation();
+  const [sections, setSections] = useState([]);
+
+  useEffect(() => {
+    const fetchFile = async () => {
+      try {
+        const module = await import(`@/../public/docs${url}${slug}/sections.ts`);
+        setSections(module.sections);
+      } catch (error) {
+        console.error("Error fetching file:", error);
+      }
+    };
+
+    fetchFile();
+  }, [slug]);
+
+  return (
+    <>
+      {sections.length !== 0 && (
+        <>
+          <p className="font-bold mb-3">{t("ui.subtitle.on_this_page")}</p>
+          <ul className="text-sm flex flex-col gap-y-2">
+            {sections.map((el, index) => (
+              <li key={index}>{el}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
+}
