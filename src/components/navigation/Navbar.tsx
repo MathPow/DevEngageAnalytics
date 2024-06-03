eb286ca0dfd98f601bb9bc06922ce01bdc4ec760
Mathys <mathys.deshaies@icloud.com>
2024-06-03 15:55:22 -0400

    fix: add base path const

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -27,28 +27,28 @@ import { BASE_PATH } from "@/lib/composables/production";
 const components: { title: string; href: string; description: string }[] = [
   {
     title: linkListComponentsEnum.allInOneDev,
-    href: `${BASE_PATH}/docs/components/${formatSlug(linkListComponentsEnum.allInOneDev)}`,
+    href: `${BASE_PATH}docs/components/${formatSlug(linkListComponentsEnum.allInOneDev)}`,
     description: "A modal dialog that interrupts the user with important content and expects a response.",
   },
   {
     title: linkListComponentsEnum.gitLover,
-    href: `${BASE_PATH}/docs/components/${formatSlug(linkListComponentsEnum.gitLover)}`,
+    href: `${BASE_PATH}docs/components/${formatSlug(linkListComponentsEnum.gitLover)}`,
     description: "For sighted users to preview content available behind a link.",
   },
   {
     title: linkListComponentsEnum.allInOneDesigner,
-    href: `${BASE_PATH}/docs/components/${formatSlug(linkListComponentsEnum.allInOneDesigner)}`,
+    href: `${BASE_PATH}docs/components/${formatSlug(linkListComponentsEnum.allInOneDesigner)}`,
     description:
       "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
   },
   {
     title: linkListComponentsEnum.certificatesFlex,
-    href: `${BASE_PATH}/docs/components/${formatSlug(linkListComponentsEnum.certificatesFlex)}`,
+    href: `${BASE_PATH}docs/components/${formatSlug(linkListComponentsEnum.certificatesFlex)}`,
     description: "Visually or semantically separates content.",
   },
   {
     title: linkListComponentsEnum.beautifulAsymmetric,
-    href: `${BASE_PATH}/docs/components/${formatSlug(linkListComponentsEnum.beautifulAsymmetric)}`,
+    href: `${BASE_PATH}docs/components/${formatSlug(linkListComponentsEnum.beautifulAsymmetric)}`,
     description: "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
   },
 ];
