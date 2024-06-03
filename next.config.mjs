6e237729cf83e16886b1eacbf6081845fd699397
Mathys <mathys.deshaies@icloud.com>
2024-06-03 15:33:28 -0400

    fix: deploy test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/DevEngageAnalytics" : "",
+  basePath: process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_PATH : "",
   images: {
     unoptimized: true,
   },
