26264d7db98e32d5f3d1687c361153807a25beeb
Mathys <mathys.deshaies@icloud.com>
2024-06-02 23:37:05 -0400

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/my-app" : "",
   images: {
     unoptimized: true,
   },
