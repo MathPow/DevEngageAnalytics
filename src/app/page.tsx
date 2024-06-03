cebcabf97f412bb2a9aa93ae08b5545bad4dd109
Mathys <mathys.deshaies@icloud.com>
2024-06-03 17:19:48 -0400

    deploy text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <p className="top-24 mb-12 mt-12 text-center">-- {process.env.NODE_ENV} --</p>
         {theme === "dark" ? <LogoDark className="size-96" /> : <Logo className="size-96" />}
         <p className="mt-6 animate-pulse text-center font-bold">Project in progress...</p>
-        <Spline scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode" />
+        {/* <Spline scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode" /> */}
       </div>
     </main>
   );
