8725cbdd1eef83a7ad90e0fb87bb1eb19f9d6bd3
Mathys <mathys.deshaies@icloud.com>
2024-06-04 14:44:59 -0400

    feat: make footer translanted

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 import BlackCasinoChip from "@/../public/assets/img/casino-chip.png";
 import RedCasinoChip from "@/../public/assets/img/red-casino-chip.png";
 import BlueCasinoChip from "@/../public/assets/img/blue-casino-chip.png";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
+  const { t } = useTranslation();
+
   return (
     <div className="border-t border-t-_bgLightGray py-2">
       <div className="mx-[12vw] flex max-w-[1300px] items-center gap-x-2 2xl:mx-auto">
@@ -13,11 +16,11 @@ export default function Footer() {
           <Image className="size-8" src={BlackCasinoChip} alt={"black casino chip"} />
         </div>
         <p className="text-sm text-_darkGrayText dark:text-_lightGrayText">
-          Built by{" "}
+          {t("ui.footer.built_by")}{" "}
           <a className="underline" href="https://www.linkedin.com/in/mathys-deshaies/" target="_blank">
             Mathys Deshaies
           </a>
-          . The source code is available on{" "}
+          . {t("ui.footer.the_source_code_is_available_on")}{" "}
           <a className="underline" href="https://github.com/MathPow/DevEngageAnalytics" target="_blank">
             GitHub
           </a>
