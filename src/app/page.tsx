3e81c827f672750983a3da9d319abe68059369ad
Mathys <mathys.deshaies@icloud.com>
2024-06-03 22:13:23 -0400

    feat: add SplineViewer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 import Logo from "@/../public/logo.svg";
 import LogoDark from "@/../public/LogoDark.svg";
 import { useTheme } from "next-themes";
-import Spline from "@splinetool/react-spline";
+import React from "react";
+import SplineViewer from "@/components/SplineViewer";
 
 export default function Home() {
   const { theme } = useTheme();
@@ -14,8 +15,8 @@ export default function Home() {
         <p className="top-24 mb-12 mt-12 text-center">-- {process.env.NODE_ENV} --</p>
         {theme === "dark" ? <LogoDark className="size-96" /> : <Logo className="size-96" />}
         <p className="mt-6 animate-pulse text-center font-bold">Project in progress...</p>
-        {/* <Spline scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode" /> */}
       </div>
+      <SplineViewer />
     </main>
   );
 }
---
+++ src/components/SplineViewer.tsx
@@ -0,0 +1,6 @@
+import Spline from "@splinetool/react-spline";
+import React from "react";
+
+export default function SplineViewer() {
+  return <Spline className="z-10 w-10" scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode" />;
+}
