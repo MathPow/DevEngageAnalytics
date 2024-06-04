0a61f77d697cedbf41a91cceb070faae7ee7bcf4
Mathys <mathys.deshaies@icloud.com>
2024-06-03 22:13:50 -0400

    fix:

--- src/lib/composables/production.ts
+++ src/lib/composables/production.ts
@@ -1,1 +1,1 @@
-export const BASE_PATH = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_PATH : "/";
+export const BASE_PATH = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_PATH + "/" : "/";
