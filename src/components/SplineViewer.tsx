4a23bee9ea12c2be0618ee68c40df333f039e17f
Mathys <mathys.deshaies@icloud.com>
2024-06-05 11:21:59 -0400

    fix: place spline to center of page

--- src/components/SplineViewer.tsx
+++ src/components/SplineViewer.tsx
@@ -2,5 +2,10 @@ import Spline from "@splinetool/react-spline";
 import React from "react";
 
 export default function SplineViewer() {
-  return <Spline className="z-10 w-10" scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode" />;
+  return (
+    <Spline
+      className="absolute left-0 right-0 top-0 -z-10 w-10"
+      scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode"
+    />
+  );
 }
