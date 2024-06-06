6e8b98fca7be4b9fa46b92f204dc3dcbf4d1cb0a
Mathys <mathys.deshaies@icloud.com>
2024-06-06 15:04:56 -0400

    feat: add scroll system for docnav

--- src/components/docs/DocPage.tsx
+++ src/components/docs/DocPage.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import DocSections from "@/components/docs/DocSections";
 import DocContent from "@/components/docs/DocContent";
 import DocNav from "@/components/navigation/DocNav";
+import { useEffect, useRef, useState } from "react";
 
 interface DocPageProps {
   slug: string;
@@ -8,15 +11,44 @@ interface DocPageProps {
 }
 
 export default function DocPage({ slug, url }: DocPageProps) {
+  const [scrollY, setScrollY] = useState(0);
+  const docContentRef = useRef<HTMLDivElement>(null);
+  const docNavRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    const newScrollY = window.scrollY;
+    console.log(window.outerHeight);
+    if (
+      newScrollY < window.outerHeight * 0.2 - 100 &&
+      docNavRef.current &&
+      docContentRef.current &&
+      docNavRef.current.clientHeight < docContentRef.current.clientHeight
+    ) {
+      setScrollY(newScrollY);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="mx-[12vw] mt-28 flex max-w-[1300px] flex-row gap-x-8 2xl:mx-auto">
-      <div className="hidden w-56 sm:block">
+    <div className="mx-[12vw] mt-[calc(20vh-150px+5vw)] flex max-w-[1300px] flex-row gap-x-8 2xl:mx-auto">
+      <div
+        ref={docNavRef}
+        className={`scrollbar sticky top-24 hidden w-64 overflow-hidden hover:overflow-y-auto sm:block`}
+        style={{ height: `calc(75vh + ${scrollY}px)` }}
+      >
         <DocNav />
       </div>
-      <div className="w-full">
+      <div ref={docContentRef} className="w-full">
         <DocContent slug={slug} url={url} />
       </div>
-      <div className="ml-10 hidden w-56 lg:block">
+      <div className="sticky top-24 ml-10 hidden h-96 w-56 lg:block">
         <DocSections slug={slug} url={url} />
       </div>
     </div>
