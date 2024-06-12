565eb3b2ea7989129644e6b5d2f689442dc85517
Mathys <mathys.deshaies@icloud.com>
2024-06-12 15:06:46 -0400

    feat: add the new BusinessDevCard to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import SplineViewer from "@/components/SplineViewer";
 import { Button } from "@/components/ui/button";
 import Icon from "@/components/Icon";
@@ -8,9 +8,18 @@ import Link from "next/link";
 import { BASE_PATH } from "@/lib/composables/production";
 import BackgroundGradient from "@/components/deco/BackgroundGradient";
 import { useTranslation } from "react-i18next";
+import DevCard from "@/components/cards/dev/DevCard";
+import { DevCardEnum } from "@/lib/types/devCardEnum";
+import { Optional } from "@/lib/types/optional";
 
 export default function Home() {
   const { t } = useTranslation();
+  const demoUsernameRef = useRef<HTMLInputElement>(null);
+  const [demoUsername, setDemoUsername] = useState<Optional<string>>();
+
+  function handleUsernameOnClick() {
+    if (demoUsernameRef.current) setDemoUsername(demoUsernameRef.current.value);
+  }
 
   return (
     <main>
@@ -60,7 +69,11 @@ export default function Home() {
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
           ></path>
         </svg>
-        <div className="h-96 bg-[#D64444]"></div>
+        <div className="h-96 bg-[#D64444]">
+          <DevCard cardType={DevCardEnum.BusinessCard} githubUsername={demoUsername} />
+          <input ref={demoUsernameRef} />
+          <button onClick={handleUsernameOnClick}>Make card!</button>
+        </div>
       </div>
       <SplineViewer />
     </main>
