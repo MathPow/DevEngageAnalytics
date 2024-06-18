4573e3338f0db2b46df0775b1b376f2fb52b4273
Mathys <mathys.deshaies@icloud.com>
2024-06-17 20:40:28 -0400

    feat: add playgroundPage

---
+++ src/components/playground/PlaygroundPage.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import PlaygroundActions from "./PlaygroundActions";
+import PlaygroundSettings from "./PlaygroundSettings";
+import PlaygroundPreview from "./PlaygroundPreview";
+import { getAllQueryParamsAsComponent } from "@/lib/composables/getParams";
+import { Optional } from "@/lib/types/optional";
+import { Component } from "@/lib/types/component";
+
+export default function PlaygroundPage() {
+  const [scrollY, setScrollY] = useState(0);
+  const docContentRef = useRef<HTMLDivElement>(null);
+  const docNavRef = useRef<HTMLDivElement>(null);
+  const [selectedType, setSelectedType] = useState<Optional<Component>>();
+  const [userInfoEntered, setUserInfoEntered] = useState<any>();
+  const [userInfoFetched, setUserInfoFetched] = useState<any>();
+
+  const handleScroll = () => {
+    const newScrollY = window.scrollY;
+    if (
+      newScrollY < window.outerHeight * 0.2 - 100 &&
+      docNavRef.current &&
+      docContentRef.current &&
+      docNavRef.current.clientHeight < docContentRef.current.clientHeight
+    ) {
+      setScrollY(newScrollY);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    setSelectedType(getAllQueryParamsAsComponent(window.location.href));
+  }, []);
+
+  return (
+    <div className="mx-[6vw] mt-[calc(20vh-150px+5vw)] flex max-w-[1300px] flex-col gap-x-8 sm:flex-row md:mx-[12vw] 2xl:mx-auto">
+      <div
+        ref={docNavRef}
+        style={{ height: `calc(75vh + ${scrollY}px)` }}
+        className={`scrollbar top-24 w-full flex-shrink-0 overflow-hidden rounded-xl border bg-_lightBg p-4 shadow-md hover:overflow-y-auto dark:bg-_darkBg sm:w-20 lg:w-64`}
+      >
+        <PlaygroundSettings selectedType={selectedType} setUserInfoEntered={setUserInfoEntered} />
+      </div>
+      <div ref={docContentRef} className="w-full">
+        <PlaygroundActions setSelectedType={setSelectedType} />
+        <PlaygroundPreview
+          type={selectedType?.type}
+          format={selectedType?.format}
+          userInfoEntered={userInfoEntered}
+          userInfoFetched={userInfoFetched}
+          setUserInfoFetched={setUserInfoFetched}
+        />
+      </div>
+    </div>
+  );
+}
