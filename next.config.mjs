804db54817a06f30cfb08b85d8ffc9ffc4e00b0b
Mathys <mathys.deshaies@icloud.com>
2024-07-28 15:41:34 -0400

    build: test vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   basePath: process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_PATH : "",
   images: {
     unoptimized: true,
