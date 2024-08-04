b987ff69f7b5ec2954bb0e617af3f697fbc47d7c
Mathys <mathys.deshaies@icloud.com>
2024-08-04 18:34:03 -0400

    feat: add ShowColorsEnum

---
+++ src/lib/types/showColorsEnum.ts
@@ -0,0 +1,12 @@
+export enum ShowColorsEnum {
+  Blue = "blue",
+  Green = "green",
+  Red = "red",
+  Orange = "orange",
+  Pink = "pink",
+  Purple = "purple",
+  White = "white",
+  Gray = "gray",
+}
+
+export const showColors = Object.values(ShowColorsEnum);
