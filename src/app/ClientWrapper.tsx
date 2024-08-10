99969a62024c08e51d1089c4cde3cd7df517193b
Mathys <mathys.deshaies@icloud.com>
2024-08-10 17:46:07 -0400

    fix: add themeProvider and Toaster tags

--- src/app/ClientWrapper.tsx
+++ src/app/ClientWrapper.tsx
@@ -8,6 +8,8 @@ import esTranslation from "@/../public/locales/es.json";
 import Navbar from "@/components/navigation/Navbar";
 import Footer from "@/components/navigation/Footer";
 import { useEffect, useState } from "react";
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 interface ClientWrapperProps {
   children: React.ReactNode;
@@ -38,10 +40,13 @@ export default function ClientWrapper({ children }: ClientWrapperProps) {
     <>
       {isNavShowed ? (
         <div className="flex min-h-screen flex-col">
-          <div className="fixed left-0 top-0 -z-20 h-full w-full bg-_lightBg dark:bg-_darkBg"></div>
-          <Navbar />
-          <div className="flex-grow pb-12">{children}</div>
-          <Footer />
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <div className="fixed left-0 top-0 -z-20 h-full w-full bg-_lightBg dark:bg-_darkBg"></div>
+            <Navbar />
+            <div className="flex-grow pb-12">{children}</div>
+            <Footer />
+            <Toaster />
+          </ThemeProvider>
         </div>
       ) : (
         <div>{children}</div>
