1ab5567cd6421e71e843602ad12f413ee14d9241
Mathys <mathys.deshaies@icloud.com>
2024-08-10 17:53:44 -0400

    fix: add getLanguageEnumFromString function

--- src/lib/types/languagesEnum.ts
+++ src/lib/types/languagesEnum.ts
@@ -1,3 +1,5 @@
+import { Optional } from "./optional";
+
 export enum LanguagesEnum {
   FR = "fr",
   EN = "en",
@@ -5,3 +7,10 @@ export enum LanguagesEnum {
 }
 
 export const languages = Object.values(LanguagesEnum);
+
+export function getLanguageEnumFromString(str: string): Optional<LanguagesEnum> {
+  languages.forEach((element) => {
+    if (str === element) return element;
+  });
+  return undefined;
+}
