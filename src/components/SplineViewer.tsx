43881c4ed929cba705e2ecb557321186fba293b1
Mathys <mathys.deshaies@icloud.com>
2024-08-25 16:19:11 -0400

    feat: add animation onLoad splineViewer

--- src/components/SplineViewer.tsx
+++ src/components/SplineViewer.tsx
@@ -1,12 +1,15 @@
 import Spline from "@splinetool/react-spline";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Tooltip from "./tooltip";
 import { BASE_PATH } from "@/lib/composables/production";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
 
 export default function SplineViewer() {
   const { t } = useTranslation();
   const [tooltipText, setTooltipText] = useState("");
+  const splineRef = useRef(null);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   function onMouseHover(e: { target: { name: string } }) {
     const name = e.target.name;
@@ -32,15 +35,27 @@ export default function SplineViewer() {
     }
   }
 
+  function onLoad() {
+    setTimeout(() => setIsAnimating(true), 200);
+  }
+
   return (
     <>
       <Tooltip text={tooltipText} />
-      <Spline
-        className="absolute left-0 right-0 top-0 z-10"
-        onMouseHover={onMouseHover}
-        onMouseDown={onMouseDown}
-        scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode"
-      />
+      <motion.div
+        ref={splineRef}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: isAnimating ? 1 : 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Spline
+          className="absolute left-0 right-0 top-0 z-10"
+          onMouseHover={onMouseHover}
+          onMouseDown={onMouseDown}
+          scene="https://prod.spline.design/cEGK4BVdk2pDyxvF/scene.splinecode"
+          onLoad={onLoad}
+        />
+      </motion.div>
     </>
   );
 }
